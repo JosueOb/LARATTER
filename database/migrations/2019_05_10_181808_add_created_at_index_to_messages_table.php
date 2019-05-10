bc34be6465005211c926563d014993a90c36ce77
JosueOb <josuericardocando@hotmail.com>
2019-05-10 13:28:07 -0500

    Corriendo Migraciones

---
+++ database/migrations/2019_05_10_181808_add_created_at_index_to_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedAtIndexToMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            // Por convención se indica primero el nombre de la table, la columna e index.
+            $table->dropIndex('messages_created_at_index');
+        });
+    }
+}
